431b42b64214f14a2e691e7398e59303dca24d63
Nikhil <nikhilbarahate1996@gmail.com>
2022-11-19 20:43:38 +0530

    first Commit

---
+++ app1/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    pic = models.ImageField(upload_to='image/')
+    report = models.FileField(upload_to= 'files/')
+
+    def __str__(self):
+        return self.name
